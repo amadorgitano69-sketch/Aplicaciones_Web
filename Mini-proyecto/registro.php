8da05b60194e9d1474984b8408aad460be30d6f4
marcos.garcia296 <amadorgitano69@gmail.com>
2025-11-07 10:49:27 -0500

    inicio

--- Mini-proyecto/registro.php
+++ Mini-proyecto/registro.php
@@ -1,24 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de usuario</title>
-    <link rel="stylesheet" href="/Mini-proyecto/estilos.css">
+ <meta charset="UTF-8">
+ <title>Registro de usuario</title>
+ <link rel="stylesheet" href="estilos.css">
 </head>
-<body class="fondo">
-    <h1>Registro</h1>
-    <div class="alinear-centro"> 
-    <form action="procesar_registro.php" method="post"></form>
-        <label for="Usuario:">Usuario:</label><br>
-        <input type="text" name="usuario" required><br><br>
-        <label for="Contraseña:">Contraseña:</label><br>
-        <input type="password" name="password" required><br><br>
-        <div class="boton">
-        <button type="submit">Registrarse</button>
-        </div>
-    </from>
-    <p>¿Ya tienes cuenta? <a href="lohin.php">Inicia sesion aqui</a></p>
-    </div>
+<body>
+ <h1>Registro</h1>
+ <form action="procesar_registro.php" method="post">
+ <label>Usuario:</label>
+ <input type="text" name="usuario" required><br><br>
+ <label>Contraseña:</label>
+ <input type="password" name="password" required><br><br>
+ <button type="submit">Registrarse</button>
+ </form>
+ <p>¿Ya tienes cuenta? <a href="login.php">Inicia sesión aquí</a></p>
 </body>
 </html>
