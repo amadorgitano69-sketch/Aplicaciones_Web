c62acfe10318f194cf41bc3cb9223e981a17fff9
marcos.garcia296 <amadorgitano69@gmail.com>
2025-11-12 13:41:56 -0500

    Primeros pasos

--- Proyecto/registro.php
+++ Proyecto/registro.php
@@ -3,14 +3,14 @@
 <head>
  <meta charset="UTF-8">
  <title>Registro de usuario</title>
- <link rel="stylesheet" href="estilos.css">
+ <link rel="stylesheet" href="original.css">
 </head>
 <body>
  <h1>Registro</h1>
  <form action="procesar_registro.php" method="post">
- <label>Usuario:</label>
+ <label>Usuario:</label><br>
  <input type="text" name="usuario" required><br><br>
- <label>Contraseña:</label>
+ <label>Contraseña:</label><br>
  <input type="password" name="password" required><br><br>
  <button type="submit">Registrarse</button>
  </form>
